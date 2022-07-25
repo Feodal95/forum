4343b64f5af2ed8a39a03b205e4bde013a33df82
Khan Akhmadov <turpalkhan.akhmadov_95@mail.ru>
2022-07-25 14:14:18 +0300

    mail

--- service/views.py
+++ service/views.py
@@ -5,6 +5,8 @@ from .forms import PostForm, CommentForm, UserRegisterForm
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 
@@ -15,8 +17,9 @@ def index(req):
 def about(req):
     return render(req, 'about.html')
 
-class RegisterForm(CreateView):
+class RegisterForm(SuccessMessageMixin, CreateView):
     form_class = UserRegisterForm
+    success_message = "%(username)s was created succesfully!"
     template_name = 'register.html'
     success_url = reverse_lazy('login')
 
@@ -29,12 +32,27 @@ class DetailPostView(DetailView):
     model = Post
     template_name = "detail_post.html"
 
-class CreatePostView(PermissionRequiredMixin, CreateView):
-    permission_required = 'service.add_post'
-    model = Post
-    template_name = "create_post.html"
-    form_class = PostForm
-    # fields = "__all__"
+# class CreatePostView(PermissionRequiredMixin, CreateView):
+#     permission_required = 'service.add_post'
+#     model = Post
+#     template_name = "create_post.html"
+#     form_class = PostForm
+#     # fields = "__all__"
+
+def create_post(req):
+    form = PostForm()
+    if req.method == "POST":
+        form = PostForm(req.POST)
+        if form.is_valid():
+            form.save()
+            title = form.cleaned_data.get("title")
+            if title != "POST":
+                messages.error(req, f"Something ment wrong")
+                return redirect('index')
+            # id = form.cleaned_data.get("id")
+            messages.success(req, f"Post {title} was created successfully")
+            return redirect('index')
+    return render(req, "create_post.html", {"form": form})
 
 class UpdatePostView(PermissionRequiredMixin, UpdateView):
     permission_required = 'service.change_post'
