534d709e43fc6f35d0926ced8708224f6954f70f
Khan Akhmadov <turpalkhan.akhmadov_95@mail.ru>
2022-07-31 00:02:35 +0300

    func-contr

--- api/views.py
+++ api/views.py
@@ -2,7 +2,49 @@
 from rest_framework import viewsets
 from api.models import Checkbox
 from api.serializers import CheckboxSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-class CheckboxViewSet(viewsets.ModelViewSet):
-    queryset = Checkbox.objects.all()
-    serializer_class = CheckboxSerializer
+# class CheckboxViewSet(viewsets.ModelViewSet):
+#     queryset = Checkbox.objects.all()
+#     serializer_class = CheckboxSerializer
+
+@api_view(['GET'])
+def checkbox_list(req):
+    checkboxes = Checkbox.objects.all()
+    serializer = CheckboxSerializer(checkboxes, many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def checkbox_detail(req, pk):
+    try:
+        checkbox = Checkbox.objects.get(id=pk)
+        serializer = CheckboxSerializer(checkbox)
+    except Checkbox.DoesNotExist:
+        return Response({"error": f"Checkbox with id={pk} is not found"}, status = status.HTTP_404_NOT_FOUND)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def checkbox_create(req):
+    serializer = CheckboxSerializer(data=req.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['PUT'])
+def checkbox_update(req, pk):
+    try:
+        checkbox = Checkbox.objects.get(id=pk)
+        serializer = CheckboxSerializer(checkbox, data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+    except Checkbox.DoesNotExist:
+        return Response({"error": f"Checkbox with id={pk} is not found"}, status = status.HTTP_404_NOT_FOUND)
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def checkbox_delete(req, pk):
+    checkbox = Checkbox.objects.get(id=pk)
+    checkbox.delete()
+    return Response(status = status.HTTP_204_NO_CONTENT)
